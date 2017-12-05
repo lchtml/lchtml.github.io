e9e53d289e9760f6ea02c6e91b23bff653e160a2
lchtml <34062079+lchtml@users.noreply.github.com>
2017-12-05 10:16:01 +0800

    微场景

---
+++ w微场景/js/js.js
@@ -0,0 +1,19 @@
+/**
+ * Created by lenovo on 17/12/5 005.
+ */
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
+
+
